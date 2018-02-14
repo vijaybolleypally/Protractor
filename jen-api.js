9b7fa693f96161bcd4645ad505f3d704ecdd74e2
Vijay Kumar Bolleypally <E002961@cigniti.com>
2018-02-14 17:57:07 +0530

    Jnekins finished

--- jen-api.js
+++ jen-api.js
@@ -1,1 +1,26 @@
 var jenkinsapi = require('jenkins-api');
+
+// username/password 
+var jenkins = jenkinsapi.init("http://admin1:Open@10.62.64.169:8080/jenkins/");
+
+jenkins.all_jobs(function(err, data) {
+function getStatusFromColor(color) {
+  return color.indexOf('blue') >= 0? "Pass":"Fail";
+}
+  if (err){ return console.log(err); }
+//  console.log(data[1])
+  for(var i = 0; i < data.length; i++) {
+      var obj = data[i];
+      console.log("Job : "+ obj.name+" is " + getStatusFromColor(obj.color));
+  }
+});
+
+//
+//$ node jen-api.js
+//Job : api_test is Pass
+//Job : code_deploy is Pass
+//Job : dev_unit_tests is Pass
+//Job : e2e_tests is Pass
+//Job : job_one_pass is Pass
+//Job : job_two_fail is Fail
+//Job : smoke_test is Pass
--- jen-cli.js
+++ jen-cli.js
@@ -1,10 +1,10 @@
 var JenkinsClient = require('jenkins-client')
-var jobX = JenkinsClient("http://localhost:8080/", "admin", "5a34aa4e094944a5bd1d9e046cd37880", "job_two_fail")
+var jobX = JenkinsClient("http://localhost:8080/", "admin1", "Open", "job_two_fail")
 jobX.info(getInfo)
 function getInfo (err, statusCode, payload) {
- // console.log("err:"+err)
- // console.log("statusCode:"+statusCode)
- // console.log(payload)
+  console.log("err:"+err)
+  console.log("statusCode:"+statusCode)
+  console.log(payload)
 }
 
 
